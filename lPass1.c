b56de16000c57305dace564bbb59b0f15a89dead
ShashwatHiregoudar <shashwathiregoudar@gmail.com>
2017-11-20 12:00:32 +0530

    New

---
+++ lPass1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+struct estab
+{
+char csname[10];
+char extsym[10];
+int address;
+int length;
+}es[15];
+
+int main(){
+    
+}
